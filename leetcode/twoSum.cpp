d23fdb790213e8e05dbfe343274cf24f92659c08
authyx <132447715+authyx@users.noreply.github.com>
2026-05-18 16:14:46 +0100

    fix: optimized for ONE solution

--- leetcode/twoSum.cpp
+++ leetcode/twoSum.cpp
@@ -12,19 +12,14 @@ public:
     vector<int> twoSum(vector<int>& nums, int target)
     {
         unordered_map<int, int> map;
-        vector<int> result;
         for (int i = 0; i < nums.size(); i++)
         {
             int sub = target - nums[i];
             if (map.find(sub) != map.end())
-            {
-                result.push_back(map[sub]);
-                result.push_back(i);
-                break;
-            }
+                return {map[sub],i};
             map[nums[i]] = i;
         }
-        return (result);
+        return {};
     }
 };
 
